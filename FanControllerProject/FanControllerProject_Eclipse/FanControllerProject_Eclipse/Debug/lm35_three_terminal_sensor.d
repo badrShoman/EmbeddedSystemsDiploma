6fe4490173fc24efbb3836c8629ebf942f86932d
Mohamed Badr <70198431+badrShoman@users.noreply.github.com>
2022-11-06 13:10:05 +0200

    Delete FanControllerProject directory

--- FanControllerProject/FanControllerProject_Eclipse/FanControllerProject_Eclipse/Debug/lm35_three_terminal_sensor.d
+++
@@ -1,9 +0,0 @@
-lm35_three_terminal_sensor.o lm35_three_terminal_sensor.o:  \
- ../lm35_three_terminal_sensor.c ../lm35_three_terminal_sensor.h ../adc.h \
-  ../std_types.h
-
-../lm35_three_terminal_sensor.h:
-
-../adc.h:
-
-../std_types.h:
